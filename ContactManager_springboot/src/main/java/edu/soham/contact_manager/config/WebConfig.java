977791ba5f04a36e818272523b73892567d31bc4
Sindhu <sindhureddyvari3@gmail.com>
2024-11-06 13:55:39 +0530

    initial commit

---
+++ ContactManager_springboot/src/main/java/edu/soham/contact_manager/config/WebConfig.java
@@ -0,0 +1,19 @@
+package edu.soham.contact_manager.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SuppressWarnings("unused")
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("http://localhost:3000") // Use a pattern
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+                
+    }
+}
